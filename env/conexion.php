cff901cbc93f12df02d5e86d77921243f3943330
Ambiente 209-3 <Ambiente 209-3@NEIDFPRHLFSD146.FormacionSena.RED>
2023-04-18 11:36:15 -0500

    La conexion

---
+++ env/conexion.php
@@ -0,0 +1,26 @@
+<?php
+
+class conexion
+{
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $database = "pokemon33";
+    private $con;
+
+    public function __construct()
+    {
+        try {
+            $this->con = new PDO("mysql:dbname=$this->database;host=$this->host", $this->user, $this->password);
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            echo "falla la conexion" . $e->getMessage();
+
+        }
+    }
+
+    public function getCon(){
+        
+    }
+}
--- views/formEnvPedidos.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once "header.php";
-//* body
-
-
-
-//* body
-include_once "footer.php";
