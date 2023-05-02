f8d5709fd27f860e61aeac9527de97741bf7dd89
Ambiente 209-3 <Ambiente 209-3@NEIDFPRHLFSD146.FormacionSena.RED>
2023-05-02 08:02:14 -0500

    Cambios

--- views/assets/js/roles.js
+++ views/assets/js/roles.js
@@ -1,5 +1,26 @@
 function created() {
-    alert('creando roles')
+    url = "../controllers/roles.create.php"
+
+    //* Informacion del formulario
+    var data = {
+
+    }
+
+    //* Opciones de la peticion
+    var options = {
+
+    }
+
+    fetch(url, {
+        options
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error =>{
+            console.error(`Error al crear el rol: ${error}`);
+        })
 }
 
 function readed() {
